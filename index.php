31dfe7f210b64449aecfa609e25a8aa06f2daa8d
sscc20111 <mw92559404@gmail.com>
2023-06-28 16:59:24 +0900

    타이포 마우스 모션 테스트

---
+++ index.php
@@ -0,0 +1,9 @@
+<? include ("../../common/inc/header.php"); ?>  
+<script type="module" src="./app.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+<script src="https://pixijs.download/release/pixi.js"></script>
+<style>
+    canvas {width: 100%;height: 100%;}
+    body {height: 100vh; background-color: #ffffff;}
+</style>
+<? include ("../../common/inc/footer.php"); ?>       
